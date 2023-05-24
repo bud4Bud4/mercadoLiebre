7cfef5df275ccb249325339e8dee7df4ca5da034
bud4Bud4 <tar.garcia.tomas@gmail.com>
2023-05-24 10:01:41 -0300

    agrego forms de registro y login, ademas agrego hipevinculo al home en el logo del header

--- app.js
+++ app.js
@@ -9,4 +9,5 @@ app.listen(3030, () => console.log("Server abierto en el puerto 3030"))
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./views/home.html")))
 
-
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "./views/login.html")))
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "./views/register.html")))
