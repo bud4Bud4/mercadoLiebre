d8ee8e1048fe55fa6bd6e11cc4dd4e07ac0700ee
bud4Bud4 <tar.garcia.tomas@gmail.com>
2023-06-05 20:40:19 -0300

    corrijo rutas

--- src/Routes/main.js
+++ src/Routes/main.js
@@ -2,11 +2,11 @@ let express = require('express');
 let path = require('path')
 let router = express.Router();
 
-router.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/home.html")))
+router.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "../views/main/home.html")))
 
-router.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "../views/login.html")))
+router.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, "../views/users/login.html")))
 
-router.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "../views/register.html")))
+router.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, "../views/users/register.html")))
 
 router.get('/producto/:idProducto?', (req, res) => {
     if (req.params.idProducto == undefined) {
