9eb9881fae0a5dad7e8c7d3caeb9c9062e0fea66
bud4Bud4 <tar.garcia.tomas@gmail.com>
2023-07-03 20:12:46 -0300

    Registro de usuarios implementado (Sin hash)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,8 +12,24 @@ module.exports = {
         res.render("users/register")
     },
     userRegisterProcess: (req, res) => {
-        const user = {
-            
+        console.log(req.body);
+        const user ={ 
+        "id" : datos.length + 1,
+        "name" : req.body.name,
+        "lastName" : req.body.lastName,
+        "user" : req.body.user,
+        "birthday" : req.body.birthday,
+        "country" : req.body.country,
+        "city" : req.body.city,
+        "street" : req.body.street,
+        "height" : req.body.height,
+        "detail" : req.body.detail,
+        "userProfile" : req.body.userProfile,
+        "interests" : req.body.interests,
+        "avatar" : req.body.avatar,
+        "password" : req.body.password
+        }
+        fs.writeFileSync(path.resolve(__dirname, '../database/users.json'), JSON.stringify([...datos, user], null, 2), 'utf-8');
+        res.redirect("/")
         }
     }
-}
